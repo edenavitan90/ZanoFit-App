7201218811d581baf6328fd021a952b6bd94e07f
Eden Avitan <edenavitan90@gmail.com>
2021-04-02 01:33:38 +0300

    sdfg

---
+++ src/components/navbar.component.js
@@ -0,0 +1,32 @@
+import React, { Component, component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+export default class Navbar extends Component {
+
+    render() {
+      return (
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+          
+          <Link to="/" className="navbar-brand">     </Link>
+          <div className="collpase navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            
+            <li className="navbar-item">
+            <Link to="/" className="nav-link">       </Link>
+            </li>
+            
+            <li className="navbar-item">
+            <Link to="/create" className="nav-link"> </Link>
+            </li>
+            
+            <li className="navbar-item">
+            <Link to="/user" className="nav-link">Create User</Link>
+            </li>
+          
+          </ul>
+          </div>
+        </nav>
+      );
+    }
+  }
