546dcde9fb210084f5cfb842916903aabc433a91
Eden Avitan <edenavitan90@gmail.com>
2021-08-19 20:25:59 +0300

    /

--- backend/server.js
+++ backend/server.js
@@ -34,4 +34,5 @@ app.use('/user/coach', coachUsersRouter);
 app.use('/posts', postRouter);
 //app.use('/coach', usersRouter);
 
+
 app.listen(port, () => console.log(`The server is running on port: ${port}`));
